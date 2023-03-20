7fd9935eed65cb972648b91b93071ddf59af89e1
ABU KALAM <99273096+Abukalam-web@users.noreply.github.com>
2023-03-20 09:05:23 +0600

    fyhfghfh

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const text = "Electrical And Electronic Engineering";
 const typespped  = 200 ;
 let i = 0 ;
 
+
 function typing(){
     if (i < text.length){
         document.getElementById("typing-text").innerHTML += text.charAt(i);
